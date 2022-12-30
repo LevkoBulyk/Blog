e06ef55cd7c562940e131632521bdc121501c0b5
levivanbulyk <levivanbulyk@gmail.com>
2022-12-30 18:23:21 +0100

    Initial commit

---
+++ src/HelpingModels/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Blog.HelpingModels.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
