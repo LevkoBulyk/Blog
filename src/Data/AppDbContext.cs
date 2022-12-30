0b41914ddc1ec66864559abe92859ab66cd7fdca
levivanbulyk <levivanbulyk@gmail.com>
2022-12-30 19:01:41 +0100

    Application DB context created (only Articles included)

---
+++ src/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public IEnumerable<Article> Articles { get; set; }
+        public IEnumerable<ArticleText> ArticleTexts { get; set; }
+        public IEnumerable<ArticleMedia> ArticleMedias { get; set; }
+    }
+}
